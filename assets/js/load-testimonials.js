df60a3eb3935f2000261e1a4649c5e5a8d705b57
Emil Eriksson <98emil.eriksson@gmail.com>
2025-12-27 12:58:02 +0100

    add data loading to testimonials.html

---
+++ assets/js/load-testimonials.js
@@ -0,0 +1,20 @@
+function loadHandler() {
+  fetch("../assets/data/testimonials.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const testimonial = document.createElement("div");
+        testimonial.innerText = JSON.stringify(item);
+        testimonial.innerHTML = `
+        <p class="testimonial-text">${item.message}</p>
+        <p class="testimonial-author">${item.author}</p>`;
+
+        const testimonialGallery = document.getElementById(
+          "testimonial-gallery"
+        );
+
+        testimonialGallery.appendChild(testimonial);
+      });
+    });
+}
+window.addEventListener("load", loadHandler);
