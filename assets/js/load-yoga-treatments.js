f193c97b1ab235f9dfdc38c710d538dd71e89800
Emil Eriksson <98emil.eriksson@gmail.com>
2026-01-06 11:54:54 +0100

    add book buttons to treatments

--- assets/js/load-yoga-treatments.js
+++ assets/js/load-yoga-treatments.js
@@ -2,13 +2,14 @@ function loadHandler() {
   fetch("../assets/data/treatments.json")
     .then((res) => res.json())
     .then((data) => {
-      data.treatments.yoga.forEach((item) => {
+      data.treatments.yogaWithRetreat.forEach((item) => {
         const treatment = document.createElement("div");
         treatment.classList.add("treatment-container");
         treatment.innerText = JSON.stringify(item);
         treatment.innerHTML = `
         <h3 class="treatment-name">${item.name}</h3>
-        <p class="treatment-description">${item.description}</p>`;
+        <p class="treatment-description">${item.description}</p>
+        <button href="">Boka</button>`;
 
         const treatmentGallery = document.getElementById("treatment-gallery");
 
